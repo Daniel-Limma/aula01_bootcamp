8c3ad637f9c3f927144fc75a8799e2b99f85dd8e
Daniel Lima <daniellimafs01@gmail.com>
2026-06-22 22:06:17 -0300

    adcionar exemplo_02

---
+++ exemplo_02.py
@@ -0,0 +1,13 @@
+# Crie um programa onde o usuário digite dois valores e apareça a soma.
+
+# print(int(input("Insira o primeiro número:")) + int(input("Insira o segundo número:")))
+
+# numero = 3 # Int
+# print(type(numero))
+# numero_decimal = 4.78 # flooat
+# nome_de_usuario = "Daniel" # Str
+# verdadeiro = True # False
+
+nome = input("Insira seu nome: ")
+quantidade_caracteres = len(nome)
+print(quantidade_caracteres)
