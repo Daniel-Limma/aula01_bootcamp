704d97a7e9257ce5261ec1a1e98fc38afcc4a618
Daniel Lima <daniellimafs01@gmail.com>
2026-06-22 22:07:08 -0300

    adcionar o exemplo_00

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+# Crie um programa que o usuário digita o seu nome e retorna o número de caracteres.
+
+
+#print(len(input("Insira seu nome:")))
+
+nome = "Daniel"
+print(len(nome))
+
+# Crie um programa onde o usuário digite dois valores e apareça a soma.
+
+# num1 = int(input("Insira o primeiro número"))
+# num2 = int(input("Insira o segundo número"))
+
+# soma = num1 + num2
+# print(soma)
+
